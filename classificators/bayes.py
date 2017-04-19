1afb6bbda54e47048c07cbc869ea0cf6e7e91d4d
learp <learp@yandex.ru>
2017-04-19 21:48:17 +0400

    fix svm
    add bayes
    next step: refactoring (many parts of repeated code)

--- classificators/bayes.py
+++ classificators/bayes.py
@@ -1,32 +1,16 @@
-from collections import Counter
-from sklearn import svm
+from sklearn.naive_bayes import GaussianNB
+from classificators.svm import get_words_from
 
 from classificators.common import *
 
 
-def get_words_from(articles, path_to_articles):
-    counter = Counter()
-    article_num = 0
-
-    for article_file in articles:
-        article_num += 1
-
-        article_file = open(join(path_to_articles, article_file), 'r', encoding='utf8')
-
-        text = article_file.read()
-        counter += Counter(prepare_words_from(text))
-
-    return counter
-
-
-def main(path_to_crime_articles, path_to_not_crime_articles, learn_count=100, classify_count=1000, space=400):
+def main(path_to_crime_articles, path_to_not_crime_articles, learn_count, classify_count, space=400):
     articles = get_files_from(path_to_crime_articles)
+
     articles_to_learn = articles[:learn_count]
 
-    # get words and their freq
     counter = get_words_from(articles_to_learn, path_to_crime_articles)
 
-    # make features
     i = 0
     feature_space = dict()
     for (word, count) in counter.most_common(space):
@@ -35,7 +19,6 @@ def main(path_to_crime_articles, path_to_not_crime_articles, learn_count=100, cl
 
     print(feature_space)
 
-    # transform articles to vectors in our feature space
     article_crimes_vectors = []
     for article_crime_file in articles_to_learn:
         article_file = open(join(path_to_crime_articles, article_crime_file), 'r', encoding='utf8')
@@ -65,15 +48,15 @@ def main(path_to_crime_articles, path_to_not_crime_articles, learn_count=100, cl
 
         article_not_crimes_vectors.append(article_not_crime_vector)
 
-    classifier = svm.SVC()
+    classifier = GaussianNB()
+
     X = []
     X.extend(article_crimes_vectors)
     X.extend(article_not_crimes_vectors)
 
     Y = [0] * (len(article_crimes_vectors))
     Y.extend([1] * len(article_not_crimes_vectors))
-    print(X)
-    print(Y)
+
     classifier.fit(X, Y)
 
     success_count = 0
